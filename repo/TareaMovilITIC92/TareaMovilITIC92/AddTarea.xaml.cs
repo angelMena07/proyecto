476b044fea48a07bae39c4e504bf9ab346daa825
David <angelmena9999@gmail.com>
2020-07-29 18:22:37 -0500

    proyecto

---
+++ repo/TareaMovilITIC92/TareaMovilITIC92/AddTarea.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaMovilITIC92.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaMovilITIC92
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddTarea : ContentPage
+    {
+        private TareaManager manager;
+        public AddTarea(TareaManager manager)
+        {
+            InitializeComponent();
+            this.manager = manager;
+        }
+
+        public async void OnSaveTarea(object sender, EventArgs e)
+        {
+            await manager.Add(txtTitulo.Text, txtDetalle.Text, txtMateria.Text, txtFecha.Text);
+        }
+    }
+}
---
+++ repo/TareaMovilITIC92/TareaMovilITIC92/UpdateTarea.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaMovilITIC92.Data;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaMovilITIC92
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UpdateTarea : ContentPage
+    {
+        private TareaManager manager;
+        private long Id;
+        public UpdateTarea(TareaManager manager, long id, string titulo, string detalle, string materia, string fecha)
+        {
+            InitializeComponent();
+            this.manager = manager;
+            Id = id;
+            txtTitulo.Text = titulo;
+            txtDetalle.Text = detalle;
+            txtMateria.Text = materia;
+            txtFecha.Text = fecha;
+
+        }
+
+        public async void OnUpdateTarea(object sender, EventArgs e)
+        {
+            await manager.Update(Id, txtTitulo.Text, txtDetalle.Text, txtMateria.Text, txtFecha.Text);
+        }
+    }
+}
